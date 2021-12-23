b05acca70e1760e21ca190e101840430adb4fa34
Blain Crawford <blaincrawford@gmail.com>
2021-12-23 10:41:15 -0500

    finished fourth problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...indices) {
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(!indices.includes(arr[i])){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 
 };
 
