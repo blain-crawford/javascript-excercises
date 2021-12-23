06ccff4dbe8285b66e75799d33df87fcef1c319f
Blain Crawford <blaincrawford@gmail.com>
2021-12-23 10:07:16 -0500

    figured out third question

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  let strArr = str.split('');
+  let reversedStr = '';
+  while(strArr.length > 0){
+    reversedStr += strArr.pop();
+  }
+  return reversedStr;
 };
 
 // Do not edit below this line
