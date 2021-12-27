636e63b30a5a35acdc05f22258dac198a62a7766
Blain Crawford <blaincrawford@gmail.com>
2021-12-27 10:39:14 -0500

    solved 6th problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  let leapYear = false;
+  //if year divisible by 100 and not 400
+  if(year % 100 === 0 && year % 400 !== 0) {
+    //return false
+    return leapYear;
+  //if year is divisible by 4 
+  } else if (year % 4 === 0) {
+    //make leap year true
+    leapYear = true;
+  //if year divisible by 100 and 400 return true
+  }
+  return leapYear;
 };
 
 // Do not edit below this line
