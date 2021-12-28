58fde68d6476b2d0230d4b026d59d5f2541ed439
Blain Crawford <blaincrawford@gmail.com>
2021-12-28 09:52:26 -0500

    solved the 8th problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	let counter = 0;
+  for(let i = 0; i < numArr.length; i ++){
+    counter += numArr[i];
+  }
+  return counter;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let counter = 1;
+  for(let i = 0; i < numArr.length; i ++){
+    counter *= numArr[i];
+  }
+  return counter;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0){
+    return 1;
+  }
+  let counter = 1;
+  for(let i = 1; i <= num; i++) {
+    counter *= i;
+  }
+  return counter;
 };
 
 // Do not edit below this line
