2d1e826334f1ce0b6afc89e6553a22bfa49c6029
Blain Crawford <blaincrawford@gmail.com>
2021-12-28 09:33:54 -0500

    solved the 7th probelm

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  num = (num - 32) * 5/9;
+  
+  if(num > 0 || num < 0 || num - Math.floor(num) !== 0) {
+    let roundTemp = parseFloat(num.toFixed(1));
+    return roundTemp;
+  }
+  return num;
 };
 
-const ctof = function() {
 
+const ctof = function(num) {
+  num = (num * 9/5) + 32;
+
+  if(num > 0 || num < 0 || num - Math.floor(num) !== 0) {
+    let roundTemp = parseFloat(num.toFixed(1));
+    return roundTemp;
+  }
+  return num;
 };
 
 // Do not edit below this line
