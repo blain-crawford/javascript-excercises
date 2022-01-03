572da650e11e806860ddb8a9664c6cf6b63bcac2
Blain Crawford <blaincrawford@gmail.com>
2022-01-03 15:13:16 -0500

    got through problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+let palindromes = function (string) {
+  newString = string
+    .replace(/\s/g, "")
+    .replace(/[^\w\s]|_/g, "")
+    .toLowerCase();
 
+  let stringArr = newString.split("");
+  let checkArr = newString.split("");
+  checkArr.reverse();
+
+  for (let i = 0; i < stringArr.length; i++) {
+    if (newString[i] !== checkArr[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
